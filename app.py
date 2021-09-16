763b8b65513e7736a97913adaa5f0df0b6369875
pmmc92 <pmmc92@users.noreply.github.com>
2021-09-16 17:25:24 +0100

    started working on deployment

---
+++ app.py
@@ -0,0 +1,23 @@
+
+## Load packages
+
+import pickle
+import streamlit as st
+
+## Loading Model
+
+pickle_in = open("Hearth_model.pkl", 'rb')
+classifier = pickle.load(pickle_in)
+
+## Designing app
+
+def pred_function(time,serum_sodium,ejection_fraction,age,platelets,serum_creatinine,creatinine_phosphokinase, high_blood_pressure,sex, anaemia, diabetes, smoking):
+    if Anaemia == "Yes":
+        anaemia = 1
+    else:
+        anaemia = 0
+    
+    
+
+
+    
