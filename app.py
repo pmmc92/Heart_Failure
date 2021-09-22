7d083de32e425e43fa65c8205a8a51180fdd0a20
pmmc92 <pmmc92@users.noreply.github.com>
2021-09-22 17:07:25 +0100

    Testing streamlit app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 
 import pickle
 import streamlit as st
+import pandas as pd
 
 ## Loading Model
 
@@ -11,7 +12,12 @@ classifier = pickle.load(pickle_in)
 
 ## Creating pre-processing function
 
-def pred_function(time,serum_sodium,ejection_fraction,age,platelets,serum_creatinine,creatinine_phosphokinase, high_blood_pressure,sex, anaemia, diabetes, smoking):
+Hypertension = 0
+Diabetes = 0
+Sex = 0
+Smoking = 0
+
+def pred_function(time,serum_sodium,ejection_fraction,age,platelets,serum_creatinine,creatinine_phosphokinase, Hypertension,Sex, Anaemia, Diabetes, Smoking):
     if Anaemia == "Yes":
         anaemia = 1
     else:
@@ -28,7 +34,7 @@ def pred_function(time,serum_sodium,ejection_fraction,age,platelets,serum_creati
         sex = 1
     else:
         sex = 0
-    if Smoking == "Yes":
+    if Smoking == "Smoker":
         smoking = 1
     else:
         smoking = 0
@@ -42,5 +48,27 @@ def pred_function(time,serum_sodium,ejection_fraction,age,platelets,serum_creati
     
     return predictor
 
+## Main page
 
+def main():
+    st.markdown("teste")
+
+    Sex = st.radio("Gender",("Male","Female"))
+    age = st.slider("Age", min_value = 18, max_value = 110, step = 1)
+    time = st.slider("Days After Diagnosis", min_value = 0, max_value = 100, step = 1)
+    Anaemia = st.radio("Does the patient has anaemia?",("Yes","No"))
+    Diabetes = st.radio("Does the patient has diabetes?",("Yes","No"))
+    Smoking = st.radio("Smoking status",("Smoker","Non-Smoker"))
+    platelets = st.number_input("Platelets (Kp/ml")
+    serum_sodium = st.number_input("Sodium blood level (mEq/L")
+    serum_creatinine = st.number_input("Sodium blood level (mg/dl")
+    creatinine_phosphokinase = st.number_input("CPK blood level (ug/L")
+    ejection_fraction = st.number_input("Ejection Fraction (%)")
+
+    if st.button("Predict"):
+        result = pred_function(time,serum_sodium,ejection_fraction,age,platelets,serum_creatinine,creatinine_phosphokinase, Hypertension,Sex, Anaemia, Diabetes, Smoking)
+        st.success("The death event is {}".format(result))
+
+if __name__=='__main__': 
+    main()
     
